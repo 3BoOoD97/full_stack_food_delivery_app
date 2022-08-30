ed7ef455937f2a1c030b297a350621d7c175bcde
3BoOoD97 <abodn70@hotmail.com>
2022-08-30 21:29:17 +0200

    Header Component

--- src/index.js
+++ src/index.js
@@ -5,9 +5,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 import App from './App';
 
-ReactDOM.render(
-    <Router>
-        
-<App />
-</Router>
-, document.getElementById('root'));
+ReactDOM.render(<Router>
+    <App />
+    </Router>,
+     document.getElementById('root'));

afb55c89b65594aa0614e12889d8421c46c0f67c
3BoOoD97 <abodn70@hotmail.com>
2022-09-11 18:02:38 +0200

    Display images in Home Container

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -39,7 +39,7 @@ const Header = () => {
   })
 }
   return (
-    <header className='fixed z-50 w-screen p-3 px-4 md:p-6 md:px-16'>
+    <header className='fixed z-50 w-screen p-3 px-4 md:p-6 md:px-16 bg-primary'>
         {/* Desktop and tablet */}
         <div className='hidden md:flex w-full h-full items-center justify-between'>
             <Link to={"/"}className='flex items-center gap-2'>
@@ -91,7 +91,7 @@ const Header = () => {
           </motion.div>
           )
         }
-       </div>|
+       </div>
         </div>
         </div>
         {/* Mobile */}
